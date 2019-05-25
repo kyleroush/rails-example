bb2e476fb04d71298e68b29ed8c3afe1ef5f8153
Kyle Roush <roush.kyle@gmail.com>
2019-05-25 18:28:35 -0500

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,10 @@ class Order < ApplicationRecord
 
   validates :customer_email, format: { with: URI::MailTo::EMAIL_REGEXP }
 
+  def zero
+    0
+  end
+  
   def total_amount
     line_items.reduce(0) { |acc, current| acc + current.unit_price * current.quantity }
   end
